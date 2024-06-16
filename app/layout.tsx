9105d4aff4cdeef9efcf69f78a4c11bfdd6fef60
moalmatry <moalmatry2000@gmail.com>
2024-06-16 20:43:42 +0300

    feat: add custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  // weight: ["400", "700"],
+  // variable: "--font-family-sans-serif",
+});
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
@@ -18,7 +27,9 @@ export const metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-50 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
