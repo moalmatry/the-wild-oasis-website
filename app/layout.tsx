0a669cc1d369ef9ece82e12215b6973dc041f03b
moalmatry <moalmatry2000@gmail.com>
2024-06-16 21:23:25 +0300

    faet: home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen flex flex-col`}
+        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen flex flex-col relative`}
       >
         <Header />
 
